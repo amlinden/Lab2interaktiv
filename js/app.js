952305005a405767af8d944815e9a9c87c30b18c
alexander-johansson <alexanderjohansson0303@gmail.com>
2015-02-18 14:38:04 +0100

    fixade app.js konflikt

--- js/app.js
+++ js/app.js
@@ -4,16 +4,15 @@ $(function() {
 	
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"), model);
-<<<<<<< HEAD
+
+	//amandas views
 	var headerView = new HeaderView($("#headerView"), model);
 	var startquicklyView = new StartquicklyView($("#headerView"), model);
 	var mydinnerView = new mydinnerView($("#section"), model);
 
-=======
+	//alex views
 	var myDinnerView = new MyDinnerView($("#myDinnerView"), model);
 	var ingredientsView = new IngredientsView($("#ingredientsView"), model);
 	var selectedMenuView = new SelectedMenuView($("#selectedMenuView"), model);
-=======
->>>>>>> 189d1a7ac6ffe462936b10ca1bc0a2653e2a7bda
->>>>>>> 9170805aba8452fc788692ab08afa624cdd82040
+
 });
