2e0f1e3d3c7c74817dc4f1235d832af4675d265c
amlinden <amlinden@kth.se>
2015-02-20 13:48:43 +0100

    2

--- js/view/myDinnerView.js
+++ js/view/myDinnerView.js
@@ -7,12 +7,8 @@ var MyDinnerView = function(container, model){
 	var dish = this.dish = container.find("#dish");
 
 	function viewThis(){
-		numberOfGuests.html(model.getNumberOfGuests());
-<<<<<<< HEAD
+		numberOfGuests.html(model.getNumberOfGuests(2));
 
-=======
-		dish.html(model.getDish(model.getSelectedDinner(0)).name);
->>>>>>> 56610d0a8ca1b1d4c6254db4823d25bff6a4dbb4
 	}
 
 	this.update = function(){
@@ -24,3 +20,4 @@ var MyDinnerView = function(container, model){
 	viewThis();
 }
 
+ 
