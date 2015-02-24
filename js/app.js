63d76f01ade3620367f193564b6dba311119ab1c
alexander-johansson <alexanderjohansson0303@gmail.com>
2015-02-24 12:54:21 +0100

    bort med dinnermodel

--- js/app.js
+++ js/app.js
@@ -15,11 +15,10 @@ $(function() {
 	//var ingredientsView = new IngredientsView($("#ingredientsView"), model);
 	//var selectedMenuView = new SelectedMenuView($("#selectedMenuView"), model);
 	//var preparationView = new PreparationView($("#preparationView"), model);
-	//var menuOverviewView = new MenuOverviewView($("#menuOverviewView"), model);
+	var menuOverviewView = new MenuOverviewView($("#menuOverviewView"), model);
 
 	//controlers
 	//var mydinnercontroler = new Mydinnercontroler(myDinnerView,model)
 	//var menuOverviewController = new MenuOverviewController(menuOverviewView, model);
-	var menuOverviewView = new MenuOverviewView($("#menuOverviewView"), model);
 
 });
