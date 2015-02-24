7b559740fa644ee2e67958bfb380f45d88bb3dbd
alexander-johansson <alexanderjohansson0303@gmail.com>
2015-02-24 12:45:36 +0100

    Revert "dishesview"
    This reverts commit f5ffd41d851b9dbe385ea22aae04c5d926cd5531.

--- js/controler/dishescontroler.js
+++
@@ -1,9 +0,0 @@
-var Mydishescontroler = function(view, model){
-	
-	view.coursebutton.click(function(){
-		model.getAllDishes(model.getDish());
-
-		});
-}
-
-//typeselectcoursebutton
--- js/view/dishesView.js
+++ js/view/dishesView.js
@@ -1,18 +1,4 @@
 var DishesView = function (container, model) {
-	var allpictures = this.allpictures = containerpictures.find("#pictures");
-	var coursebutton = this.coursebutton = container1.find("#coursebutton");
-	var starter1 = this.starter1 = containerpictures.find("#starter1");
-
-
-	function viewThis(){
-		allpictures.html(model.getNumberOfGuests());
-	}
-
-	this.update = function(){
-		viewThis();
-	}
-
-	model.addObserver(this);
-
-	viewThis();
+	this.pictures = row.find("#row"); // eller col-sm-6 col-md-3??
+	this.pictures.html(model.getAllDishes());
 }
