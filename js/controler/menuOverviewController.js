650c3f038bf59b9ae86210f02accd673671f7fb7
amlinden <amlinden@kth.se>
2015-02-25 10:36:59 +0100

    Nu funkar första stidan

--- js/controler/menuOverviewController.js
+++ js/controler/menuOverviewController.js
@@ -2,10 +2,5 @@ var MenuOverviewController = function (view, model){
 	view.finishButton.click(function(){
 		$("#menuOverviewView").hide();
 	});
-	//ska hidea menuoverviewview och hämta ??ALEX?
-	view.confirmDishButton.click(function(){
-		$("#menuOverviewView").hide();
-		$("#exampleView").hide();
-		$("#preparationView").show();
-	});
+
 }
