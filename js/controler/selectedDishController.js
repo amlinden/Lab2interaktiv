042d2d6a7504f85157b3da0ba07588e8e38f3abc
amlinden <amlinden@kth.se>
2015-03-10 16:07:04 -0300

    s<d

--- js/controler/selectedDishController.js
+++ js/controler/selectedDishController.js
@@ -10,7 +10,9 @@ var SelectedDishController = function(view, model){
 	});
 
 	view.confirmDishButton.click(function(){
+
 		model.addDishToMenu(model.getDishToView());
+
 		$("#selecteddish").hide();
 		$("#dishesview").show();
 		$("#thirdview").show();
